4e1178a0fddb5411c6946ba24bc0da9d99b60db2
Faisal Zamir <jafri.site@gmail.com>
2025-11-06 09:31:28 +0500

    CSRF_TRUSTED_ORIGINS added

--- ticket_classifier/settings.py
+++ ticket_classifier/settings.py
@@ -108,6 +108,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['https://customer-ticket-classification.onrender.com']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
